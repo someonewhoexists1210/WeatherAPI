db69fc2b6342f45ecb59ed592ab33a5d43338147
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-10 18:30:01 +0530

    added more routes

--- api/calls.py
+++ api/calls.py
@@ -16,15 +16,50 @@ def today(location):
         return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
     return response.json()
 
-def daily(location, date):
-    date_obj = datetime.strptime(date, '%Y-%m-%d')
-    new_date_obj = date_obj + timedelta(days=7)
+def xrange(location, startDate, days):
+    date_obj = datetime.strptime(startDate, '%Y-%m-%d')
+    new_date_obj = date_obj + timedelta(days=days-1)
     new_date_str = new_date_obj.strftime('%Y-%m-%d')    
-    url = main_url + f'{location}/{date}/{new_date_str}?include=days&unitGroup=metric&lang=en&key={api_key}'
+    url = main_url + f'{location}/{startDate}/{new_date_str}?include=days&unitGroup=metric&lang=en&key={api_key}'
     response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
     return response.json()
 
 def day(location, date):
     url = main_url + f'{location}/{date}?&unitGroup=metric&lang=en&key={api_key}'
     response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
     return response.json()
+
+def next(location, key, number=1):
+    url = main_url + f'{location}/next{number}{key}?unitGroup=metric&lang=en&key={api_key}'
+    response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
+    return response.json()
+
+def last(location, key, number=1):
+    if key == 'hours':
+        number = 24
+    url = main_url + f'{location}/last{number}{key}?unitGroup=metric&lang=en&key={api_key}'
+    response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
+    return response.json()
+
+def tty(location, key):
+    url = main_url + f'{location}/{key}?&unitGroup=metric&lang=en&key={api_key}'
+    response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
+    return response.json()
+
+def to_date(location, key):
+    url = main_url + f'{location}/{key}?&unitGroup=metric&lang=en&key={api_key}'
+    response = requests.get(url)
+    if not response.ok:
+        return {'errorCode': response.status_code, 'message': response.text.split(':')[-1]}
+    return response.json()
+
--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from .calls import today, daily, day
+from .calls import *
 
 
 # Create your views here.
 def weather(request):
-    return HttpResponse('Hello, World!')
+    return today_view(request)
 
 def today_view(request):
     location = request.GET.get('location')
@@ -27,11 +27,49 @@ def day_view(request):
             return render(request, 'error.html', {'errorCode': data['errorCode'], 'error': data['message']})
         return render(request, 'error.html', {'errorCode': 500, 'error': 'Server Error'})
         
-    return render(request, 'result.html', {'data': data})
+    return render(request, 'day.html', {'data': data})
 
-def daily_view(request):
+def range_view(request):
     location = request.GET.get('location')
     date1 = request.GET.get('date1')
-    data = daily(location, date1)
+    data = xrange(location, date1)
 
-    return JsonResponse(data)
+    return render(request, 'daily.html', {'data': data})
+
+def next_view(request):
+    location = request.GET.get('location')
+    key = request.GET.get('key')
+    number = int(request.GET.get('number', 1))
+    data = next(location, key, number)
+    return dynamic_view(request, data, key)
+
+
+def last_view(request):
+    location = request.GET.get('location')
+    key = request.GET.get('key')
+    number = int(request.GET.get('number', 1))
+    data = last(location, key, number)
+    return dynamic_view(request, data, key)
+
+
+def tty_view(request):
+    location = request.GET.get('location')
+    key = request.GET.get('key')
+    data = tty(location, key)
+    return dynamic_view(request, data, key)
+
+
+def to_date_view(request):
+    location = request.GET.get('location')
+    key = request.GET.get('key')
+    data = to_date(location, key)
+    
+    return dynamic_view(request, data, key)
+
+def dynamic_view(request, data, key):
+    if 'errorCode' in data:        
+        if 400 <= data['errorCode'] < 500:
+            return render(request, 'error.html', {'errorCode': data['errorCode'], 'error': data['message']})
+        return render(request, 'error.html', {'errorCode': 500, 'error': 'Server Error'})
+        
+    return render(request, 'daily.html', {'data': data, 'key': key})
