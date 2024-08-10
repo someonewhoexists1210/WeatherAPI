346fe6881fbb8aedbdf46e00f78d7722a9d87ec5
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-10 21:54:10 +0530

    added basic form validation

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -32,17 +32,47 @@ function updateFormFields() {
     }
 }
 
-function disableHiddenFields() {
-    const fields = document.querySelectorAll('#weatherForm input, #weatherForm select');
-    fields.forEach(field => {
-        if (field.style.display === 'none' || field.closest('div').style.display === 'none') {
-            field.disabled = true;
-        } else {
-            field.disabled = false;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('weatherForm').addEventListener('submit',  (event) => {
+        event.preventDefault();
+        let filled = true;
+        const fields = document.querySelectorAll('#weatherForm input, #weatherForm select');
+        fields.forEach(field => {
+            if (field.style.display === 'none' || field.closest('div').style.display === 'none') {
+                field.disabled = true;
+            } else {
+                field.disabled = false;
+                if (field.value === '') {
+                    filled = false;
+                }
+                if (field.type === 'date') {
+                    const currentDate = new Date();
+                    const selectedDate = new Date(field.value);
+                    const oneYearAgo = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate());
+                    const fifteenDaysLater = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 15);
+
+                    if (selectedDate < oneYearAgo || selectedDate > fifteenDaysLater) {
+                        filled = false;
+                        alert("Please select a date within the range of 1 year ago and 15 days later.");
+                    }
+                }
+                if (field.type === 'number') {
+                    if (field.value < 1) {
+                        filled = false;
+                        alert("Please enter a number greater than 0.");
+                    }
+                    if (field.value > 15) {
+                        filled = false;
+                        alert("Please enter a number less than 16.");
+                    }
+                }
+            }
+        });
+        if (filled) {
+            event.target.submit();
+        } else{
+            alert("Please fill in all inputs")
         }
     });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('weatherForm').addEventListener('submit', disableHiddenFields);
 });
+
