674073c9e9f48112246b810cdfddd4ba42a98d40
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-11 17:43:11 +0530

    added user db

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/user/login/')
 def home(request):
     return render(request, 'index.html')
--- weather/settings.py
+++ weather/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+AUTH_USER_MODEL = 'user.CustomUser'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'frontend',
+    'user',
 ]
 
 MIDDLEWARE = [
