27cab7dbef78048d31036e0f02662c3a8fb03762
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-12 17:32:19 +0530

    added alerts

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var city = document.getElementById('city');
+    var latitude = parseFloat(document.getElementById('latitude').innerHTML);
+    var longitude = parseFloat(document.getElementById('longitude').innerHTML);
     if (city.innerHTML === 'None') {
-        fetch('/api/get_city')
+        fetch('/api/get_city?lat=' + latitude + '&lon=' + longitude)
             .then(response => response.json())
             .then(data => {
                 city.innerHTML = data.city;
